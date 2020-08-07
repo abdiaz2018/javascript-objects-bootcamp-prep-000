ab4b1a219f4a37d0c598b32c52cb560d1c2dc9fe
Abraham Diaz <abdiaz2018@gmail.com>
2020-08-07 21:49:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,15 @@
-var playlist = new object();
+
+var playlist = new Object({
+breakfast: "toast"
+})
+
+function updatePlaylist(playlist, artistName, songTitle) {
+var artist = artistName
+playlist[artist] = songTitle
+return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+delete playlist[artistName]
+return playlist
+}
